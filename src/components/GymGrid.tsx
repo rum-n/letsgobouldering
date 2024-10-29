4f43fac319ecfc2b56840f686950d66420f7cc32
Rumen <rumen.mnv@protonmail.com>
2024-10-29 21:40:27 +0200

    prep gym page

--- src/components/GymGrid.tsx
+++ src/components/GymGrid.tsx
@@ -4,51 +4,52 @@ import { Gym } from "@/types/Gym";
 import GymCard from "./GymCard";
 
 interface GymCardProps {
-    gyms: Gym[];
+  gyms: Gym[];
 }
 
 const GymGridContainer = styled.div`
-    min-height: 100vh;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 2rem;
-    margin: 1rem;
-    padding: 0 5rem;
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 2rem;
+  margin: 1rem;
+  padding: 0 5rem;
 `;
 
-
-
 const GymGrid = ({ gyms }: GymCardProps) => {
-    const [followedGymIds, setFollowedGymIds] = useState<number[]>([]);
+  const [followedGymIds, setFollowedGymIds] = useState<number[]>([]);
 
-    useEffect(() => {
-        const fetchFollowedGyms = async () => {
-            try {
-                const res = await fetch('/api/user/follows');
-                if (res.ok) {
-                    const data = await res.json();
-                    setFollowedGymIds(data.map((gym: Gym) => gym.id));
-                }
-            } catch (error) {
-                console.error('Error fetching followed gyms:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchFollowedGyms = async () => {
+      try {
+        const res = await fetch("/api/user/follows");
+        if (res.ok) {
+          const data = await res.json();
+          setFollowedGymIds(data.map((gym: Gym) => gym.id));
+        }
+      } catch (error) {
+        console.error("Error fetching followed gyms:", error);
+      }
+    };
 
-        fetchFollowedGyms();
-    }, []);
+    fetchFollowedGyms();
+  }, []);
 
-    const isFollowingGym = useCallback((gym: Gym) => {
-        return followedGymIds.some((id) => id === gym.id);
-    }, [followedGymIds]);
+  const isFollowingGym = useCallback(
+    (gym: Gym) => {
+      return followedGymIds.some((id) => id === gym.id);
+    },
+    [followedGymIds],
+  );
 
-
-    return (
-        <GymGridContainer>
-            {gyms && gyms.map((gym) => (
-                <GymCard key={gym.id} gym={gym} isFollowing={isFollowingGym(gym)} />
-            ))}
-        </GymGridContainer>
-    );
-}
+  return (
+    <GymGridContainer>
+      {gyms &&
+        gyms.map((gym) => (
+          <GymCard key={gym.id} gym={gym} isFollowing={isFollowingGym(gym)} />
+        ))}
+    </GymGridContainer>
+  );
+};
 
 export default GymGrid;
--- src/pages/gyms/[id].tsx
+++ src/pages/gyms/[id].tsx
@@ -1,7 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import prisma from "../../lib/prisma";
-import Image from "next/image";
+import styled from "styled-components";
 
 interface Gym {
     id: number;
@@ -15,6 +15,54 @@ interface GymPageProps {
     gym: Gym;
 }
 
+const GymPageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem;
+`
+
+const GymHeader = styled.div`
+    width: 50%;
+    height: 300px;
+    border-radius: 8px;
+    margin-bottom: 1rem;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 8px;
+    }`
+
+const GymNameWrapper = styled.div`
+    text-align: center;
+    background-color: #fff;
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 30%;
+    box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 3rem 2rem;
+
+    h1 {
+        margin: 0;
+    }`
+
+const BackButton = styled.button`
+    background-color: #fff;
+    border: 1px solid #000;
+    border-radius: 8px;
+    padding: 0.7rem 1.2rem;
+    margin-bottom: 1rem;
+    position: absolute;
+    left: 1rem;
+    top: 1rem;
+`
+
 const GymPage = ({ gym }: GymPageProps) => {
     const router = useRouter();
 
@@ -23,12 +71,13 @@ const GymPage = ({ gym }: GymPageProps) => {
     }
 
     return (
-        <div>
-            <h1>{gym.name}</h1>
+        <GymPageWrapper>
+            <BackButton onClick={() => router.back()}>Back</BackButton>
+            <GymHeader>{gym.image && <img src={gym.image} alt={gym.name} />}</GymHeader>
+            <GymNameWrapper><h1>{gym.name}</h1></GymNameWrapper>
             <p>{gym.location}</p>
             {gym.description && <p>{gym.description}</p>}
-            {gym.image && <Image src={gym.image} alt={gym.name} />}
-        </div>
+        </GymPageWrapper>
     );
 };
 
